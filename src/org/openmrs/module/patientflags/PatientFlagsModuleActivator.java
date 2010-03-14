49f3bd59b52704ac3c4b8f94e17af08bab7d25db
Mark Goodrich <mgoodrich@pih.org>
2010-03-14 19:42:52 +0000

    patientflags: removed auto-registering of logic tokens; updated to version 1.2.6

--- src/org/openmrs/module/patientflags/PatientFlagsModuleActivator.java
+++ src/org/openmrs/module/patientflags/PatientFlagsModuleActivator.java
@@ -22,7 +22,6 @@ import org.apache.commons.logging.LogFactory;
 
 import org.openmrs.GlobalProperty;
 import org.openmrs.api.context.Context;
-import org.openmrs.logic.util.LogicUtil;
 import org.openmrs.module.Activator;
 import org.openmrs.module.Extension;
 import org.openmrs.module.Module;
@@ -106,9 +105,6 @@ public class PatientFlagsModuleActivator implements Activator {
 			tmpExtensions.add(ext);
 			ModuleFactory.getExtensionMap().put(extId, tmpExtensions);
 		}
-		
-		// register the logic tokens
-		LogicUtil.registerDefaultRules();
 		
 		log.info("Starting Patient Flags Module");
 	}

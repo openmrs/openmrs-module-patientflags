e70040c8f734c990172bcb55d01bbf91287714f9
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2017-12-15 14:32:35 -0800

    MK: Renaming resource to reflect model class anme

--- omod/src/main/java/org/openmrs/module/patientflags/web/rest/FlagResource.java
+++ omod/src/main/java/org/openmrs/module/patientflags/web/rest/FlagResource.java
@@ -10,7 +10,7 @@ import org.openmrs.module.webservices.rest.web.response.ResponseException;
 
 @Resource(name = RestConstants.VERSION_1 + PatientFlagsRestController.PATIENT_FLAGS_REST_NAMESPACE + "/flag", supportedClass = Flag.class, supportedOpenmrsVersions = {
         "1.9.*", "1.10.*", "1.11.*", "1.12.*", "2.0.*" })
-public class PatientFlagResource extends MetadataDelegatingCrudResource<Flag>{
+public class FlagResource extends MetadataDelegatingCrudResource<Flag>{
 
 	public Flag newDelegate() {
 		// TODO Auto-generated method stub

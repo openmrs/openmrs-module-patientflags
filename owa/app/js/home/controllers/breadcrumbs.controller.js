0f10c18e7145d3a8144686388b2887ce2445956e
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2017-12-16 16:21:37 +0200

    Remove unneeded components

--- owa/app/js/home/controllers/breadcrumbs.controller.js
+++ owa/app/js/home/controllers/breadcrumbs.controller.js
@@ -2,7 +2,7 @@ class BreadCrumbsController {
   constructor() {
     var vm = this;
     vm.links = {};
-    vm.links["Home Components"] = "";
+    vm.links["Manage Flags"] = "";
   }
 }
 
--- owa/app/js/home/home.js
+++ owa/app/js/home/home.js
@@ -1,15 +1,7 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import breadcrumbsComponent from './components/breadcrumbs.component.js';
-import breadcrumbsPatientCreateComponent from './components/breadcrumbsPatientCreate.component';
-import translateComponent from './components/translate.component.js';
 import headerComponent from './components/header.component.js';
-import notificationComponent from './components/notification.component.js';
-import patientSearchComponent from './components/patientSearch.component.js';
-import encounterComponent from './components/encounter.component.js';
-import observationComponent from './components/observation.component.js';
-import providerComponent from './components/provider.component.js';
-import patientCreateComponent from './components/patientCreate.component.js';
 import flagsComponent from '../flags/flags.component';
 import flagsCreateComponent from '../flags/flagsCreate.component';
 import uicommons from 'openmrs-contrib-uicommons';
@@ -22,11 +14,6 @@ let homeModule = angular.module('home', [ uiRouter, 'openmrs-contrib-uicommons']
         $stateProvider.state('home', {
             url: '/',
             template: require('./home.html')
-        });
-
-        $stateProvider.state('createPatient', {
-            url: '/create-patient',
-            template: require('./patientCreatePage.html')
         });
 
         $stateProvider.state('createFlag', {
@@ -45,15 +32,7 @@ let homeModule = angular.module('home', [ uiRouter, 'openmrs-contrib-uicommons']
     }])
 
     .component('breadcrumbsComponent', breadcrumbsComponent)
-    .component('breadcrumbsPatientCreateComponent', breadcrumbsPatientCreateComponent)
-    .component('translateComponent', translateComponent)
     .component('headerComponent', headerComponent)
-    .component('notificationComponent', notificationComponent)
-    .component('patientSearchComponent', patientSearchComponent )
-    .component('encounterComponent', encounterComponent)
-    .component('observationComponent', observationComponent)
-    .component('providerComponent', providerComponent)
-    .component('patientCreateComponent', patientCreateComponent)
     .component('flags', flagsComponent)
     .component('flagsCreateComponent', flagsCreateComponent);
 

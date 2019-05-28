3d1d56fe5492875cd3e2a27537bf4760c75ec08d
Lubwama Samuel <lubwamasamuel3@gmail.com>
2019-05-29 00:57:28 +0300

    FLAG-49 Improve loading speed of Patient Dashboard
    [maven-release-plugin] prepare for next development iteration
    
    Resolve conflicts
    
    Changing from oracle JDK to open JDK
    
    Adding Spinner
    
    Changing from a post to get

--- omod/src/main/java/org/openmrs/module/patientflags/fragment/controller/PatientflagsDashboardFragmentController.java
+++ omod/src/main/java/org/openmrs/module/patientflags/fragment/controller/PatientflagsDashboardFragmentController.java
@@ -9,10 +9,10 @@ import org.openmrs.api.context.Context;
 import org.openmrs.module.patientflags.Flag;
 import org.openmrs.module.patientflags.Tag;
 import org.openmrs.module.patientflags.api.FlagService;
+import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.annotation.FragmentParam;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -22,25 +22,32 @@ public class PatientflagsDashboardFragmentController {
     protected final Log log = LogFactory.getLog(getClass());
 
     public void controller(FragmentModel model, @FragmentParam("patientId") Patient patient) {
+
+        model.addAttribute("patientId", patient.getPatientId());
+    }
+
+    public SimpleObject processPatientFlags(@FragmentParam("patientId") Patient patient) {
         FlagService flagService = Context.getService(FlagService.class);
         User user = Context.getAuthenticatedUser();
         Set<Role> userRoles = user.getRoles();
-
         List<Flag> listAllFlags = flagService.generateFlagsForPatient(patient);
-        List<Flag> resultList = new ArrayList<Flag>();
+        StringBuilder flagsString = new StringBuilder();
 
         for (Flag flag : listAllFlags) {
             for (Tag tag : flag.getTags()) {
                 Set<Role> intersection = new HashSet<Role>(tag.getRoles());
                 intersection.retainAll(userRoles);
                 if (intersection.size() > 0) {
-                    resultList.add(flag);
+                    flagsString.append("<li><span style='").append(flag.getPriority().getStyle()).append("'>").append(flag.evalMessage(patient.getPatientId())).append("</span></li>");
                     continue;
                 }
             }
         }
 
-        model.addAttribute("flags", resultList);
-    }
+        SimpleObject simpleObject = new SimpleObject();
 
+        simpleObject.put("", flagsString);
+
+        return simpleObject;
+    }
 }
--- omod/src/main/webapp/fragments/patientflagsDashboard.gsp
+++ omod/src/main/webapp/fragments/patientflagsDashboard.gsp
@@ -1,15 +1,26 @@
-<openmrs:require privilege="View Patients" otherwise="/login.htm" redirect="/index.htm" />
+<openmrs:require privilege="View Patients" otherwise="/login.htm" redirect="/index.htm"/>
 
 <div class="info-section">
     <div class="info-header">
-        <i class="icon-flag"></i><h3>${ ui.message("patientflags.patientOverview.title") }</h3>
+        <i class="icon-flag"></i>
+
+        <h3>${ui.message("patientflags.patientOverview.title")}</h3>
     </div>
 
     <div class="info-body">
-    	<ul>
-            <% flags.each { flag -> %>
-                <li><span style=" ${ flag.priority.style } "> ${ flag.evalMessage(config.patientId) } </span></li>
-            <% } %>
-    	</ul>
+        <script>
+            jq.get('${ ui.actionLink("processPatientFlags") }', {
+                patientId: ${patientId}
+            }, function (response) {
+                if (!response) {
+                    ${ ui.message("coreapps.none ") }
+                } else {
+                    jq("#flags").html(response.replace("{=", "").replace("}", ""));
+                }
+            });
+        </script>
+        <ul id="flags">
+            <i class="icon-spinner"></i>
+        </ul>
     </div>
 </div>

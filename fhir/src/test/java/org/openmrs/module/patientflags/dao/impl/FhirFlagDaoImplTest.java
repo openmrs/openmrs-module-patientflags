74d14f7098ac2eed7ad2aa68b68947a3bbe93859
ManojLL <manojlakshan421@gmail.com>
2024-07-22 17:42:17 +0530

    FM2-640: added flag dao test

---
+++ fhir/src/test/java/org/openmrs/module/patientflags/dao/impl/FhirFlagDaoImplTest.java
@@ -0,0 +1,39 @@
+package org.openmrs.module.patientflags.dao.impl;
+
+import org.hibernate.SessionFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.openmrs.module.fhir2.TestFhirSpringConfiguration;
+import org.openmrs.module.patienttflags.dao.impl.FhirFlagDaoImpl;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration(classes = TestFhirSpringConfiguration.class, inheritLocations = false)
+public class FhirFlagDaoImplTest extends BaseModuleContextSensitiveTest {
+
+    protected static final String XML_DATASET_PATH = "org/openmrs/module/patientflags/include/";
+
+    private static final String TEST_DATASET_FILE = XML_DATASET_PATH + "patientflagtest-dataset.xml";
+
+    @Autowired
+    @Qualifier("sessionFactory")
+    private SessionFactory sessionFactory;
+
+    private FhirFlagDaoImpl fhirFlagDao;
+
+    @Before
+    public void setup() throws Exception {
+
+        fhirFlagDao = new FhirFlagDaoImpl();
+        fhirFlagDao.setSessionFactory(sessionFactory);
+        executeDataSet(TEST_DATASET_FILE);
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
